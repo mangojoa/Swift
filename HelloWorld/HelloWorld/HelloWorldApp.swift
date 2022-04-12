720ea0151e9f8fae65977af00f9219c917a02a96
이두산 <sxfc675@gmail.com>
2022-04-13 00:30:08 +0900

    first commit

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 이두산 on 2022/04/13.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
