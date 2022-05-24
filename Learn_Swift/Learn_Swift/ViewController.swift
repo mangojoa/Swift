f59bea038269b329024514d0d2b22b2665eae347
이두산 <sxfc675@gmail.com>
2022-05-24 23:20:27 +0900

    outlet 변수와 Action 메소드의 생성 및 원리와 개념

--- Learn_Swift/Learn_Swift/ViewController.swift
+++ Learn_Swift/Learn_Swift/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var uiTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sayHello(_ sender: Any) {
+        
+        self.uiTitle.text = "Hello, World!"
+    }
 }
 
