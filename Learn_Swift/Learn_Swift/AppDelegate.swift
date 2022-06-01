0e90881cd53ce560018a65cb6a2969cc877b127c
이두산 <sxfc675@gmail.com>
2022-06-01 22:32:50 +0900

    [22.06.01] app을 실행시키는 func application() 설명 및 지연 코드 작성

--- Learn_Swift/Learn_Swift/AppDelegate.swift
+++ Learn_Swift/Learn_Swift/AppDelegate.swift
@@ -10,10 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    /*
+     앱이 처음 실행될때, 필요한 시스템적 처리를 모두 끝내고 메인 화면을 표시하기 직전에 호출됩니다.
+     앱이 맨 처음 실행될 때 시작 화면이 모바일 기기의 스크린에 표시된 후 이 메소드가 호출되고, 이 메소드 내부에 작성된 내용이 모두 실행되고 나면
+     앞서 구현한 Main.storyboard 파일의 화면이 스크린에 표시된다.
+     */
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // 시작 화면에 조작(?)을 위해 코드하나를 추가해보자
+        sleep(5)
+        
         return true
     }
 
